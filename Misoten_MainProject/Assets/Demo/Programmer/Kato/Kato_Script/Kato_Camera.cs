da4e2d3905cbd8c60f706e8d6fe0b126c491a04b
KatoShinji <oisii333@outlook.jp>
2024-10-25 12:25:02 +0900

    Camera/Effect/Animbrend/ect

--- Misoten_MainProject/Assets/Demo/Programmer/Kato/Kato_Script/Kato_Camera.cs
+++ Misoten_MainProject/Assets/Demo/Programmer/Kato/Kato_Script/Kato_Camera.cs
@@ -1,28 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.Mathematics;
 using UnityEngine;
-using UnityEngine.Windows;
-using static UnityEngine.GraphicsBuffer;
 
-public class Camera : MonoBehaviour
+public class Kato_Camera : MonoBehaviour
 {
-    [SerializeField, Header("プレイヤーデータ")]
     public GameObject player;   //プレイヤー情報格納用
-    [SerializeField, Header("カメラフカン時に使う敵データ")]
+    private Vector3 offset;      //相対距離取得用
     public GameObject enemy;
-    private bool PushFlg_A = false;//L押下フラグ
-
-    [SerializeField, Header("カメラフカン角度 (30,30,0)")]
-    public Vector3 CameraRot= new Vector3(30.0f,30.0f,0.0f);
-    [SerializeField, Header("カメラフカン距離 (-3,4,-4)")]
-    public Vector3 CameraPos = new Vector3(-3.0f, 4.0f, -4.0f);
+    private bool PushFlg_A = false;//Rスティック押下フラグ
+    private bool PushFlg_RTr = false;//Lスティック押下フラグ
 
     // Start is called before the first frame update
     void Start()
     {
         //カメラの向きを変更する
         transform.rotation = player.transform.rotation;
+        transform.position = player.transform.position - transform.forward * 3;
     }
 
     // Update is called once per frame
@@ -37,42 +30,52 @@ public class Camera : MonoBehaviour
         if (UnityEngine.Input.GetKeyUp("joystick button 0"))
         {
             PushFlg_A = false;
+
         }
+
+        if (UnityEngine.Input.GetKeyDown("joystick button 9"))
+        {
+            PushFlg_RTr = true;
+        }
+        //Lを離した時に押し込みフラグをfalseにする
+        if (UnityEngine.Input.GetKeyUp("joystick button 9"))
+        {
+            PushFlg_RTr = false;
+
+        }
+
+
 
         GameObject obj = GameObject.Find("sword_test(Clone)");
 
-        if (obj != null) 
+        if (obj != null)
         {
             if (PushFlg_A)
             {
-                    transform.rotation = enemy.transform.rotation;
-                    transform.Rotate(CameraRot);
-                    transform.position = enemy.transform.position + enemy.transform.forward * CameraPos.z;
-                    transform.position = transform.position + enemy.transform.right * CameraPos.x;
-                    transform.position = transform.position + enemy.transform.up * CameraPos.y;
+                transform.rotation = enemy.transform.rotation;
+                transform.Rotate(30, 30, 0);
+                transform.position = enemy.transform.position - enemy.transform.forward * 4;
+                transform.position = transform.position - enemy.transform.right * 3;
+                transform.position = transform.position + enemy.transform.up * 4;
             }
             else
             {
                 transform.position = obj.transform.position - transform.forward * 5;
                 transform.position = (new Vector3(transform.position.x, 2.0f, transform.position.z));
             }
-
-            //UnityEditor.EditorApplication.isPaused = true;
-
-
-
         }
         else
         {
-            transform.rotation = player.transform.rotation;
+           
 
-            transform.position = player.transform.position - transform.forward * 3;
+            if (PushFlg_RTr)
+            {
+                transform.rotation = player.transform.rotation;
+                transform.position = player.transform.position - transform.forward * 3;
+            }
+
+              
             transform.position = (new Vector3(transform.position.x, 2.0f, transform.position.z));
         }
-
-
-
-
-
     }
 }
