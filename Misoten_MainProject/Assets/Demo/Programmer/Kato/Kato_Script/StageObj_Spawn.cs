d01fee0b5ce0611565f9b8777972bad2f4ffd552
KatoShinji <oisii333@outlook.jp>
2024-12-11 00:48:49 +0900

    位置調整

--- Misoten_MainProject/Assets/Demo/Programmer/Kato/Kato_Script/StageObj_Spawn.cs
+++ Misoten_MainProject/Assets/Demo/Programmer/Kato/Kato_Script/StageObj_Spawn.cs
@@ -13,10 +13,10 @@ public class StageObj_Spawn : MonoBehaviour
     public Vector3 SpawnObjScale;
 
     [Header("円状に設置するオブジェクトの半径")]
-    public float spinRadius = 10f; // 円の半径（インスペクタで編集可能）
+    public float SpawnObjRadius ; // 円の半径（インスペクタで編集可能）
 
     [Header("配置開始角度ずらし")]
-    public float SetobjRot = 0f; // 円の半径（インスペクタで編集可能）
+    public float SetobjRot; // 円の半径（インスペクタで編集可能）
 
     // Start is called before the first frame update
     void Start()
@@ -39,10 +39,10 @@ public class StageObj_Spawn : MonoBehaviour
         for (int i = 0; i < 6; i++)
         {
             float angleInRadians = Mathf.Deg2Rad * (i * 60+ SetobjRot); // 60度間隔で分割
-            float attackX = Mathf.Cos(angleInRadians) * spinRadius;
-            float attackZ = Mathf.Sin(angleInRadians) * spinRadius;
+            float SetPosX = Mathf.Cos(angleInRadians) * SpawnObjRadius;
+            float SetPosZ = Mathf.Sin(angleInRadians) * SpawnObjRadius;
 
-            SpawnPoints[i] = new Vector3(attackX, 0.0f, attackZ);
+            SpawnPoints[i] = new Vector3(SetPosX, 0.0f, SetPosZ);
         }
     }
 
