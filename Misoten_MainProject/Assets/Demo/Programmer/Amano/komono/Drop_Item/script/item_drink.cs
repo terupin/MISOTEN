64d592457fa07dd7f1f34ea3e631bc782db847b0
teru <teru020927@gmail.com>
2024-12-21 21:39:07 +0900

    アイテムが消えない処理の修正

--- Misoten_MainProject/Assets/Demo/Programmer/Amano/komono/Drop_Item/script/item_drink.cs
+++ Misoten_MainProject/Assets/Demo/Programmer/Amano/komono/Drop_Item/script/item_drink.cs
@@ -5,30 +5,43 @@ using UnityEngine;
 public class item_drink : MonoBehaviour
 {
     private GameObject player_obj;
+    private bool first_get;
 
     // Start is called before the first frame update
     void Start()
     {
         player_obj = GameObject.FindWithTag("Player");
+        first_get = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float itempos = this.GetComponent<Throwmove>().progress;
+
+        if (itempos > 1.0f && first_get)
+        {
+            GetItem();
+            Destroy(this.gameObject);
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "ItemSenser")
+        if (other.gameObject.tag == "ItemSenser"&&first_get)
         {
-            Debug.Log("“–‚½‚Á‚½");
-
-            Kato_Status_P Armor_meny = player_obj.gameObject.GetComponent<Kato_Status_P>();
-
-            Armor_meny.Armor++;
-
+            GetItem();
             Destroy(this.gameObject);
         }
+    }
+    private void GetItem()
+    {
+        first_get = false;
+
+        Debug.Log("“–‚½‚Á‚½");
+
+        Kato_Status_P Armor_meny = player_obj.gameObject.GetComponent<Kato_Status_P>();
+
+        Armor_meny.Armor++;
     }
 }
